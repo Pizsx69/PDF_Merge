55d0823537abff22f3f40d586160f43be02d5466
Pizsx69 <bodeakrisz@gmail.com>
2022-07-20 09:45:58 +0200

    Fixing EmbeddedAssembly

--- PDF_Merge/Program.cs
+++ PDF_Merge/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿
+using DesktopPet;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,9 +17,21 @@ namespace PDF_Merge
         [STAThread]
         static void Main()
         {
+
+            string resource1 = "PDF_Merge.PdfSharp.Charting.dll";
+            string resource2 = "PDF_Merge.PdfSharp.dll";
+            EmbeddedAssembly.Load(resource1, "PdfSharp.Charting.dll");
+            EmbeddedAssembly.Load(resource2, "PdfSharp.dll");
+
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+        static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            return EmbeddedAssembly.Get(args.Name);
         }
     }
 }
