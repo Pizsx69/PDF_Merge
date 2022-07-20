acd6002c69b89b28b2649e58d674ada8c36a19cb
Pizsx69 <bodeakrisz@gmail.com>
2022-07-20 09:16:15 +0200

    Upload project

---
+++ PDF_Merge/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace PDF_Merge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Tallozas = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Merge = new System.Windows.Forms.Button();
+            this.le = new System.Windows.Forms.Button();
+            this.fel = new System.Windows.Forms.Button();
+            this.pizsx69 = new System.Windows.Forms.Label();
+            this.clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Tallozas
+            // 
+            this.Tallozas.Location = new System.Drawing.Point(12, 151);
+            this.Tallozas.Name = "Tallozas";
+            this.Tallozas.Size = new System.Drawing.Size(171, 27);
+            this.Tallozas.TabIndex = 0;
+            this.Tallozas.Text = "Tallózás";
+            this.Tallozas.UseVisualStyleBackColor = true;
+            this.Tallozas.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 11);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(351, 134);
+            this.listBox1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Merge
+            // 
+            this.Merge.Location = new System.Drawing.Point(192, 151);
+            this.Merge.Name = "Merge";
+            this.Merge.Size = new System.Drawing.Size(171, 27);
+            this.Merge.TabIndex = 3;
+            this.Merge.Text = "Egyesítés";
+            this.Merge.UseVisualStyleBackColor = true;
+            this.Merge.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // le
+            // 
+            this.le.Location = new System.Drawing.Point(369, 86);
+            this.le.Name = "le";
+            this.le.Size = new System.Drawing.Size(50, 23);
+            this.le.TabIndex = 4;
+            this.le.Text = "button3";
+            this.le.UseVisualStyleBackColor = true;
+            this.le.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // fel
+            // 
+            this.fel.Location = new System.Drawing.Point(369, 42);
+            this.fel.Name = "fel";
+            this.fel.Size = new System.Drawing.Size(50, 23);
+            this.fel.TabIndex = 5;
+            this.fel.Text = "button4";
+            this.fel.UseVisualStyleBackColor = true;
+            this.fel.Click += new System.EventHandler(this.fel_Click);
+            // 
+            // pizsx69
+            // 
+            this.pizsx69.AutoSize = true;
+            this.pizsx69.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pizsx69.ForeColor = System.Drawing.Color.Gainsboro;
+            this.pizsx69.Location = new System.Drawing.Point(371, 9);
+            this.pizsx69.Name = "pizsx69";
+            this.pizsx69.Size = new System.Drawing.Size(48, 13);
+            this.pizsx69.TabIndex = 6;
+            this.pizsx69.Text = "Pizs\\x69";
+            this.pizsx69.Click += new System.EventHandler(this.pizsx69_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(369, 151);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(50, 23);
+            this.clear.TabIndex = 7;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 185);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.le);
+            this.Controls.Add(this.pizsx69);
+            this.Controls.Add(this.fel);
+            this.Controls.Add(this.Merge);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Tallozas);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PDF Egyesítés";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Tallozas;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button Merge;
+        private System.Windows.Forms.Button le;
+        private System.Windows.Forms.Button fel;
+        private System.Windows.Forms.Label pizsx69;
+        private System.Windows.Forms.Button clear;
+    }
+}
+
---
+++ PDF_Merge/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PdfSharp;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace PDF_Merge
+{
+    public partial class Form1 : Form
+    {
+        List<string> pdfs = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            le.Text =char.ConvertFromUtf32(8595);
+            fel.Text =char.ConvertFromUtf32(8593);
+            Merge.Visible = false;
+            clear.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.openFileDialog1.Filter = "Pdf Files|*.pdf";
+
+            // Allow the user to select multiple images.
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Title = "My Image Browser";
+
+            DialogResult dr = this.openFileDialog1.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+
+                
+
+                foreach (String file in openFileDialog1.FileNames)
+                {
+                    // Create a PictureBox.
+                    try
+                    {
+                        //pdfs.Add(file);
+                        listBox1.Items.Add(file);
+                    }
+                    catch (SecurityException ex)
+                    {
+                        // The user lacks appropriate permissions to read files, discover paths, etc.
+                        MessageBox.Show("Security error. Please contact your administrator for details.\n\n" +
+                            "Error message: " + ex.Message + "\n\n" +
+                            "Details (send to Support):\n\n" + ex.StackTrace
+                        );
+                    }
+                    catch (Exception ex)
+                    {
+                        // Could not load the image - probably related to Windows file system permissions.
+                        MessageBox.Show("Cannot display the image: " + file.Substring(file.LastIndexOf('\\'))
+                            + ". You may not have permission to read the file, or " +
+                            "it may be corrupt.\n\nReported error: " + ex.Message);
+                    }
+                }
+                Merge.Visible = true;
+                clear.Visible = true;
+
+
+            }
+        }
+        public static void MergePDFs(string targetPath, List<string> pdfs)
+        {
+            using (var targetDoc = new PdfDocument())
+            {
+                foreach (var pdf in pdfs)
+                {
+                    using (var pdfDoc = PdfReader.Open(pdf, PdfDocumentOpenMode.Import))
+                    {
+                        for (var i = 0; i < pdfDoc.PageCount; i++)
+                            targetDoc.AddPage(pdfDoc.Pages[i]);
+                    }
+                }
+                targetDoc.Save(targetPath);
+            }
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MoveItem(1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (String file in openFileDialog1.FileNames)
+            {
+                pdfs.Add(file);
+            }
+            MergePDFs("Output.pdf", pdfs);
+
+        }
+
+        private void fel_Click(object sender, EventArgs e)
+        {
+            MoveItem(-1);
+        }
+
+        public void MoveItem(int direction)
+        {
+            if (listBox1.SelectedItem == null || listBox1.SelectedIndex < 0)
+                return; 
+
+
+            int newIndex = listBox1.SelectedIndex + direction;
+
+            if (newIndex < 0 || newIndex >= listBox1.Items.Count)
+                return; 
+
+            object selected = listBox1.SelectedItem;
+
+
+            listBox1.Items.Remove(selected);
+            listBox1.Items.Insert(newIndex, selected);
+           
+            listBox1.SetSelected(newIndex, true);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pizsx69_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Pizsx69");
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            pdfs.Clear();
+            Merge.Visible = false;
+        }
+    }
+}
