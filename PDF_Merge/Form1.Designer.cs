93e6fbd85c3848bcde9a3dc168a404e8395053aa
Pizsx69 <bodeakrisz@gmail.com>
2022-07-24 08:31:39 +0200

    Fix.

--- PDF_Merge/Form1.Designer.cs
+++ PDF_Merge/Form1.Designer.cs
@@ -44,9 +44,10 @@
             // 
             // Tallozas
             // 
-            this.Tallozas.Location = new System.Drawing.Point(12, 175);
+            this.Tallozas.Location = new System.Drawing.Point(16, 215);
+            this.Tallozas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Tallozas.Name = "Tallozas";
-            this.Tallozas.Size = new System.Drawing.Size(171, 27);
+            this.Tallozas.Size = new System.Drawing.Size(228, 33);
             this.Tallozas.TabIndex = 0;
             this.Tallozas.Text = "Tallózás";
             this.Tallozas.UseVisualStyleBackColor = true;
@@ -55,9 +56,11 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 11);
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(16, 14);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(351, 134);
+            this.listBox1.Size = new System.Drawing.Size(467, 164);
             this.listBox1.TabIndex = 2;
             // 
             // openFileDialog1
@@ -66,9 +69,10 @@
             // 
             // Merge
             // 
-            this.Merge.Location = new System.Drawing.Point(191, 175);
+            this.Merge.Location = new System.Drawing.Point(255, 215);
+            this.Merge.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Merge.Name = "Merge";
-            this.Merge.Size = new System.Drawing.Size(171, 27);
+            this.Merge.Size = new System.Drawing.Size(228, 33);
             this.Merge.TabIndex = 3;
             this.Merge.Text = "Egyesítés";
             this.Merge.UseVisualStyleBackColor = true;
@@ -76,9 +80,10 @@
             // 
             // le
             // 
-            this.le.Location = new System.Drawing.Point(369, 86);
+            this.le.Location = new System.Drawing.Point(492, 106);
+            this.le.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.le.Name = "le";
-            this.le.Size = new System.Drawing.Size(50, 23);
+            this.le.Size = new System.Drawing.Size(67, 28);
             this.le.TabIndex = 4;
             this.le.Text = "button3";
             this.le.UseVisualStyleBackColor = true;
@@ -86,9 +91,10 @@
             // 
             // fel
             // 
-            this.fel.Location = new System.Drawing.Point(369, 42);
+            this.fel.Location = new System.Drawing.Point(492, 52);
+            this.fel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.fel.Name = "fel";
-            this.fel.Size = new System.Drawing.Size(50, 23);
+            this.fel.Size = new System.Drawing.Size(67, 28);
             this.fel.TabIndex = 5;
             this.fel.Text = "button4";
             this.fel.UseVisualStyleBackColor = true;
@@ -99,18 +105,20 @@
             this.pizsx69.AutoSize = true;
             this.pizsx69.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.pizsx69.ForeColor = System.Drawing.Color.Gainsboro;
-            this.pizsx69.Location = new System.Drawing.Point(371, 9);
+            this.pizsx69.Location = new System.Drawing.Point(497, 11);
+            this.pizsx69.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.pizsx69.Name = "pizsx69";
-            this.pizsx69.Size = new System.Drawing.Size(48, 13);
+            this.pizsx69.Size = new System.Drawing.Size(56, 16);
             this.pizsx69.TabIndex = 6;
             this.pizsx69.Text = "Pizs\\x69";
             this.pizsx69.Click += new System.EventHandler(this.pizsx69_Click);
             // 
             // clear
             // 
-            this.clear.Location = new System.Drawing.Point(368, 175);
+            this.clear.Location = new System.Drawing.Point(491, 215);
+            this.clear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.clear.Name = "clear";
-            this.clear.Size = new System.Drawing.Size(50, 27);
+            this.clear.Size = new System.Drawing.Size(67, 33);
             this.clear.TabIndex = 7;
             this.clear.Text = "Clear";
             this.clear.UseVisualStyleBackColor = true;
@@ -118,35 +126,38 @@
             // 
             // outfilename
             // 
-            this.outfilename.Location = new System.Drawing.Point(74, 149);
+            this.outfilename.Location = new System.Drawing.Point(99, 183);
+            this.outfilename.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.outfilename.Name = "outfilename";
-            this.outfilename.Size = new System.Drawing.Size(244, 20);
+            this.outfilename.Size = new System.Drawing.Size(324, 22);
             this.outfilename.TabIndex = 8;
             this.outfilename.Text = "Outputfile";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 152);
+            this.label1.Location = new System.Drawing.Point(16, 187);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.Size = new System.Drawing.Size(68, 16);
             this.label1.TabIndex = 9;
             this.label1.Text = "Fájl neve: ";
             // 
             // textBox1
             // 
             this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(324, 149);
+            this.textBox1.Location = new System.Drawing.Point(432, 183);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(39, 20);
+            this.textBox1.Size = new System.Drawing.Size(51, 22);
             this.textBox1.TabIndex = 10;
             this.textBox1.Text = ".pdf";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(430, 209);
+            this.ClientSize = new System.Drawing.Size(573, 257);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.outfilename);
@@ -159,6 +170,7 @@
             this.Controls.Add(this.Tallozas);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
